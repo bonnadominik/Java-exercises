f15ec02415d6ca9321db5d0445969945e63752aa
Dominik Bonna <bonna.dominik@gmail.com>
2020-03-02 18:34:57 +0100

    Third example: Palindroms checker

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ import java.util.Arrays;
 import java.util.Collections;
 public class Main {
 
-    public static void LetterCounter(String someString){
-        int finder = someString.length() - 1;
-        int counter = 0;
-        for (int i=0; i<someString.length();i++) {
-            if (someString.charAt(i)==someString.charAt(finder)){
-                counter++;
-            }
+    public static void PalindromsChecker(String someString){
+        someString = someString.replaceAll("\\s+","" ).toLowerCase();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=someString.length()-1;i>=0;i--) {
+            stringBuilder.append(someString.charAt(i));
         }
-        System.out.println(counter);
+        if (someString.equals(stringBuilder.toString())){
+            System.out.println("Podany ciąg znaków jest palindromem!");
+        } else{
+            System.out.println("Podany ciąg znaków NIE jest palindromem.");
+        }
     }
     public static void main(String[] args)
     {
-        LetterCounter("blablablabla");
+        PalindromsChecker("kajak");
+        PalindromsChecker("Ada bzy zbada");
     }
 }
